6b41864800a23b265fe1234697b899ce268ccc3c
valeriyaxs17 <wolf.valeriya@gmail.com>
2021-05-13 00:48:00 +0300

    first1

--- src/main/java/tests/CreateUserTest.java
+++ src/main/java/tests/CreateUserTest.java
@@ -1,10 +1,12 @@
 package tests;
 
 import helpers.BaseTest;
+import helpers.BaseTestThree;
+import helpers.BaseTestTwo;
 import org.testng.annotations.Test;
 import pages.AddCustPage;
 
-public class CreateUserTest extends BaseTest {
+public class CreateUserTest extends BaseTestThree {
 
     @Test
     public void createNewUser () {
